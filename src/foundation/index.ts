a69be0d29e1a54756d77929756b3cdfa5a8a4352
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-08-17 21:18:52 +0200

    Add stable foundation import surface

---
+++ src/foundation/index.ts
@@ -0,0 +1,11 @@
+export * from "./spatial.js";
+export * from "./mass-properties.js";
+export * from "./provenance.js";
+export * from "./runtime.js";
+export {
+  compareRigidMotion,
+  linearMomentum,
+  totalLinearMomentum,
+  translationalKineticEnergyJ,
+} from "./continuity.js";
+export * from "./evidence.js";
